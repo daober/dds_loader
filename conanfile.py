ed7d215656c1fd98e4aaed4a059461891638a059
daober <dan.obermaier@gmail.com>
2020-04-05 09:26:32 +0200

    removed build type from conanfile

--- conanfile.py
+++ conanfile.py
@@ -5,14 +5,14 @@ class dds_loaderConan(ConanFile):
     name = "dds_loader"
     version = "0.0.1"
     _version_range = "[~=%s]" % ".".join(version.split(".")[0:2])
-    settings = "os", "compiler", "build_type", "arch"
+    settings = "os", "compiler", "arch"
     scm = {"type": "git", "url": "auto", "revision": "auto" }
     generators = "cmake"
     no_copy_source = True
     
     def build(self):
         cmake = CMake(self)
-        cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE"] = "ON"
+        #cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE"] = "ON"
         cmake.configure()
         cmake.build()
         cmake.install()
